6b41a9deef59b3a9eb04cea7e175394c1dcc2267
scrollangel <migg.ro3@gmail.com>
2024-04-05 12:11:46 -0500

    Add navigation items to main navbar
    This commit includes the addition of the links with proper styling for store, community, user, library and downloads. Also include the go back and forward arrow buttons

--- src/app/shared/components/main-navbar/main-navbar.component.ts
+++ src/app/shared/components/main-navbar/main-navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { MainNavigationItemComponent } from '../main-navigation-item/main-navigation-item.component'
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
     selector: 'app-main-navbar',
     standalone: true,
-    imports: [MainNavigationItemComponent],
+  imports: [MainNavigationItemComponent, NgOptimizedImage],
     templateUrl: './main-navbar.component.html',
     styleUrl: './main-navbar.component.scss',
 })
--- src/app/shared/components/main-navigation-item/main-navigation-item.component.ts
+++ src/app/shared/components/main-navigation-item/main-navigation-item.component.ts
@@ -1,15 +1,30 @@
-import { Component, Input } from '@angular/core'
-import { UpperCasePipe } from '@angular/common'
-import { RouterLink, RouterLinkActive } from '@angular/router'
+import {Component, Input} from '@angular/core';
+import {NgClass, UpperCasePipe} from '@angular/common';
+import {NavigationEnd, RouterLink, RouterLinkActive} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-main-navigation-item',
-    standalone: true,
-    imports: [UpperCasePipe, RouterLink, RouterLinkActive],
-    templateUrl: './main-navigation-item.component.html',
-    styleUrl: './main-navigation-item.component.scss',
+  selector: 'app-main-navigation-item',
+  standalone: true,
+  imports: [UpperCasePipe, RouterLink, RouterLinkActive, NgClass],
+  templateUrl: './main-navigation-item.component.html',
+  styleUrl: './main-navigation-item.component.scss'
 })
 export class MainNavigationItemComponent {
-    @Input() title!: string
-    @Input() path!: string
+  @Input() title!: string;
+  @Input() path!: string;
+  isActive: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) { // Check for NavigationEnd event
+        this.updateIsActive();
+      }
+    });
+  }
+
+  updateIsActive() {
+    console.log("#router", this.router.url, this.path);
+    this.isActive = this.router.url === `/${this.path}`;
+  }
 }
